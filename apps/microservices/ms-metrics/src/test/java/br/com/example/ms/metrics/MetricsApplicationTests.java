a0aac52f033961280576565a8f234e3d6474454f
Wander Costa <wfercosta@users.noreply.github.com>
2021-11-20 15:27:27 -0300

    Project structure (#10)
    * Closes #9 - Adds project structure and basic CI
    
    * fixup! Closes #9 - Adds project structure and basic CI

---
+++ apps/microservices/ms-metrics/src/test/java/br/com/example/ms/metrics/MetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.ms.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
