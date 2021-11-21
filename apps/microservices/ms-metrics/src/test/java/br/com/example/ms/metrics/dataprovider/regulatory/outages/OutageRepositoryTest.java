789ef1be2cfc23a3c237d66417cfd1a9e5cc6faa
Wander Costa <wanderfernando@gmail.com>
2021-11-21 20:05:08 -0300

    fixup! Closes #11 - Adds outages api implementation

---
+++ apps/microservices/ms-metrics/src/test/java/br/com/example/ms/metrics/dataprovider/regulatory/outages/OutageRepositoryTest.java
@@ -0,0 +1,23 @@
+package br.com.example.ms.metrics.dataprovider.regulatory.outages;
+
+import br.com.example.ms.metrics.test.Category;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
+@Sql(value = "/db/migrations/h2/data/load.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/db/migrations/h2/data/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Tag(Category.INTEGRATION_TEST)
+public class OutageRepositoryTest {
+
+	@Autowired
+	OutageRepository sut;
+	
+}
